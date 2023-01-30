200fd18259c7b3db831b846a05e2ea3f62428bbc
PB-03-faw7 <PB-03-faw7@DC1-S-TS33-011>
2023-01-30 13:40:33 +0100

    Projektdateien hinzufügen.

---
+++ matheAufgabe/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int zahl1, zahl2, geschätzterWertI, ergebnis, richtigeAntwort = 0, falscheAntwort = 0;
+
+
+void neueZahlen()
+{
+    Random neueZahl = new Random();
+    zahl1 = neueZahl.Next(0, 101);
+    zahl2 = neueZahl.Next(0, 101);
+    ergebnis = zahl1 - zahl2;
+    Console.WriteLine("----------------------------------------------------");
+    Console.WriteLine("Bitte schreiben Sie das Ergebnis der Operation unten");
+    Console.Write(zahl1 + "-" + zahl2 + "=");
+
+    string geschätzterWertS = Console.ReadLine();
+
+    if (int.TryParse(geschätzterWertS, out geschätzterWertI))
+    {
+
+    }
+    else
+    {
+        Console.WriteLine("Der eingegebene Wert hat kein Zahlenformat.");
+    }
+
+
+}
+for (int i = 0; i < 10; i++)
+{
+
+    neueZahlen();
+    
+
+    if (geschätzterWertI == ergebnis)
+    {
+        richtigeAntwort++;
+        Console.WriteLine("Glückwunsch, richtige antwort");
+
+    }
+    else
+    {
+        falscheAntwort++;
+        Console.WriteLine("Leider, falsche Antwort");
+
+    }
+    Console.WriteLine("Bisher haben Sie {0} richtige, {1} falsche Antwort gegeben.", richtigeAntwort, falscheAntwort);
+}
+
+
+
+
+
+
+
